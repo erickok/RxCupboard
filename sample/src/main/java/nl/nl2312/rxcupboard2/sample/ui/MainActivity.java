01f0e6c99465df7f1685bc66486cde924eca489d
Eric Kok <erickok@gmail.com>
2016-11-19 00:26:07 +0100

    Updating the sample app to RxCupboard2 with RxJava2 (and without RxBinding).

--- sample/src/main/java/nl/nl2312/rxcupboard2/sample/ui/MainActivity.java
+++ sample/src/main/java/nl/nl2312/rxcupboard2/sample/ui/MainActivity.java
@@ -3,33 +3,34 @@ package nl.nl2312.rxcupboard2.sample.ui;
 import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.jakewharton.rxbinding.view.RxView;
-import com.jakewharton.rxbinding.widget.RxAdapterView;
-import com.jakewharton.rxbinding.widget.RxTextView;
-
-import java.util.List;
-
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 import nl.nl2312.rxcupboard2.OnDatabaseChange;
 import nl.nl2312.rxcupboard2.RxCupboard;
 import nl.nl2312.rxcupboard2.RxDatabase;
 import nl.nl2312.rxcupboard2.sample.CupboardDbHelper;
 import nl.nl2312.rxcupboard2.sample.R;
 import nl.nl2312.rxcupboard2.sample.model.Item;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.subscriptions.CompositeSubscription;
+
+import static io.reactivex.android.schedulers.AndroidSchedulers.mainThread;
+import static io.reactivex.schedulers.Schedulers.io;
 
 public class MainActivity extends Activity {
 
-	private CompositeSubscription subscriptions;
+	private CompositeDisposable subscriptions;
 	private ListView itemsList;
 	private EditText addEdit;
 	private Button addButton;
@@ -39,7 +40,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		subscriptions = new CompositeSubscription();
+		subscriptions = new CompositeDisposable();
 		itemsList = (ListView) findViewById(R.id.itemsList);
 		addEdit = (EditText) findViewById(R.id.addEdit);
 		addButton = (Button) findViewById(R.id.addButton);
@@ -56,83 +57,96 @@ public class MainActivity extends Activity {
 		// Load all existing items form the database into the list view
 		final ItemsAdapter adapter = new ItemsAdapter(this);
 		// Note that the underlying Cursor is created on calling query(), but individual Item objects are created when iterating (reactive pull)
-		subscriptions.add(onUi(rxCupboard.query(Item.class).toList()).subscribe(new Action1<List<Item>>() {
-			@Override
-			public void call(List<Item> items) {
-				adapter.add(items);
-				itemsList.setAdapter(adapter);
-			}
-		}, toastErrorAction));
+		subscriptions.add(
+				rxCupboard.query(Item.class)
+						.subscribeOn(io())
+						.toList()
+						.observeOn(mainThread())
+						.subscribe(items -> {
+							adapter.add(items);
+							itemsList.setAdapter(adapter);
+						}, toastErrorAction));
 
 		// Add/remove items to/from the list view on any changes in the Item database table
-		subscriptions.add(onUi(rxCupboard.changes(Item.class)).subscribe(new OnDatabaseChange<Item>() {
-			@Override
-			public void onInsert(Item entity) {
-				adapter.add(entity);
-			}
+		subscriptions.add(
+				rxCupboard.changes(Item.class)
+						.subscribeOn(io())
+						.observeOn(mainThread())
+						.subscribe(new OnDatabaseChange<Item>() {
+							@Override
+							public void onInsert(Item entity) {
+								adapter.add(entity);
+							}
 
-			@Override
-			public void onDelete(Item entity) {
-				adapter.remove(entity);
-			}
-		}, toastErrorAction));
+							@Override
+							public void onDelete(Item entity) {
+								adapter.remove(entity);
+							}
+						}, toastErrorAction));
 
 		// Remove an item from the database when it was clicked
-		subscriptions.add(onUi(RxAdapterView.itemClicks(itemsList).map(new Func1<Integer, Object>() {
-			@Override
-			public Object call(Integer position) {
-				// Return the object that was clicked
-				return adapter.getItem(position);
-			}
-		})).subscribe(rxCupboard.delete()));
+		subscriptions.add(
+				listItemClicks.map(adapter::getItem)
+						.subscribe(rxCupboard.delete()));
 
 		// Enable the Add button only when text was entered
-		subscriptions.add(onUi(RxTextView.textChanges(addEdit).map(new Func1<CharSequence, Boolean>() {
-			@Override
-			public Boolean call(CharSequence text) {
-				// Emit whether there is now any text input
-				return !TextUtils.isEmpty(text);
-			}
-		}).distinctUntilChanged()).subscribe(RxView.enabled(addButton)));
+		subscriptions.add(
+				addEditTextChanges.map(text -> !TextUtils.isEmpty(text))
+						.distinctUntilChanged()
+						.subscribe(enabled -> addButton.setEnabled(enabled)));
 
 		// Allow adding of items when pressing the Add button
-		subscriptions.add(onUi(RxView.clicks(addButton).map(new Func1<Void, String>() {
-			@Override
-			public String call(Void click) {
-				// Get the text to use for the new Item title
-				return addEdit.getText().toString();
-			}
-		}).map(new Func1<String, Item>() {
-			@Override
-			public Item call(String s) {
-				// Create the pojo
-				Item item = new Item();
-				item.title = s;
-				return item;
-			}
-		})).doOnNext(new Action1<Item>() {
-			@Override
-			public void call(Item item) {
-				// Clear input text
-				addEdit.setText(null);
-			}
-		}).subscribe(rxCupboard.put(), toastErrorAction));
+		subscriptions.add(
+				addButtonClicks.map(click -> addEdit.getText().toString())
+						.map(entry -> {
+							// Create the pojo
+							Item item = new Item();
+							item.title = entry;
+							return item;
+						})
+						.doOnNext(update -> addEdit.setText(null))
+						.subscribe(rxCupboard.put(), toastErrorAction));
 	}
 
-	private Action1<Throwable> toastErrorAction = new Action1<Throwable>() {
-		@Override
-		public void call(Throwable throwable) {
-			Toast.makeText(MainActivity.this, throwable.toString(), Toast.LENGTH_SHORT).show();
-		}
-	};
+	private Consumer<Throwable> toastErrorAction = throwable -> Toast.makeText(MainActivity.this, throwable.toString(), Toast.LENGTH_SHORT).show();
 
-	private <T> Observable<T> onUi(Observable<T> source) {
-		return source.observeOn(AndroidSchedulers.mainThread());
-	}
+	private Flowable<Integer> listItemClicks = Flowable.create((FlowableEmitter<Integer> emitter) -> {
+		AdapterView.OnItemClickListener onItemClicks = (adapterView, view, i, l) -> emitter.onNext(i);
+		emitter.setCancellable(() -> itemsList.setOnItemClickListener(null));
+		itemsList.setOnItemClickListener(onItemClicks);
+	}, BackpressureStrategy.BUFFER);
+
+	private Flowable<CharSequence> addEditTextChanges = Flowable.create((FlowableEmitter<CharSequence> emitter) -> {
+		TextWatcher addEditWatcher = new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+				emitter.onNext(charSequence);
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+			}
+		};
+		emitter.setCancellable(() -> addEdit.removeTextChangedListener(addEditWatcher));
+		addEdit.addTextChangedListener(addEditWatcher);
+		// Emit initial value
+		emitter.onNext(addEdit.getText());
+	}, BackpressureStrategy.BUFFER);
+
+	private Flowable<Object> addButtonClicks = Flowable.create((FlowableEmitter<Object> emitter) -> {
+		View.OnClickListener onAddButtonClicks = view -> emitter.onNext(new Object());
+		emitter.setCancellable(() -> addButton.setOnClickListener(null));
+		addButton.setOnClickListener(onAddButtonClicks);
+	}, BackpressureStrategy.BUFFER);
 
 	@Override
 	protected void onStop() {
 		super.onStop();
 		subscriptions.clear();
 	}
+
 }
